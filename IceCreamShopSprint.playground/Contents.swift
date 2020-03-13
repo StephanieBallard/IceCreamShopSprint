d8aff82885cda8b851d87c8217077233704edb5f
Stephanie Ballard <user.name>
2020-03-13 09:06:29 -0700

    flavors

--- IceCreamShopSprint.playground/Contents.swift
+++ IceCreamShopSprint.playground/Contents.swift
@@ -27,15 +27,29 @@ struct Cone {
 }
 
 class IceCreamShop {
-    var flavors: Flavor
-    var sizes: Size
-    var toppings: Topping
+    var flavors: [Flavor]
+    var sizes: [Size]
+    var toppings: [Topping]
     var totalSales: Double
     
-    init(flavors: Flavor, sizes: Size, toppings: Topping, totalSales: Double) {
+    init(flavors: [Flavor], sizes: [Size], toppings: [Topping], totalSales: Double) {
         self.flavors = flavors
         self.sizes = sizes
         self.toppings = toppings
-        self.totalSales = totalSales
+        self.totalSales = 0.0
     }
 }
+
+let flavor1 = Flavor(name: "Chocolate Chip Mint", rating: 4.7)
+let flavor2 = Flavor(name: "Chocolate PeanutButter Ripple", rating: 5.0)
+let flavor3 = Flavor(name: "Birthday Cake", rating: 4.9)
+let flavor4 = Flavor(name: "Vanilla Bean", rating: 3.9)
+let flavor5 = Flavor(name: "Cookie Dough", rating: 3.8)
+let flavor6 = Flavor(name: "Cookies n Cream", rating: 3.8)
+
+var flavors = [flavor1, flavor2, flavor3, flavor4, flavor5, flavor6]
+
+func listTopFlavors() {
+    
+}
+
