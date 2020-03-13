cc3a2b3a6972d27f313d326a9ebb3595dce2224b
Stephanie Ballard <user.name>
2020-03-13 10:44:44 -0700

    steps 1 - 10 working correctly

--- IceCreamShopSprint.playground/Contents.swift
+++ IceCreamShopSprint.playground/Contents.swift
@@ -1,16 +1,19 @@
 import UIKit
 
+// Step 2
 enum Size: Double {
     case small = 4.99
     case medium = 5.99
     case large = 6.99
 }
 
+// Step 1
 struct Flavor {
     var name: String
     var rating: Double
 }
 
+// Step 3
 struct Cone {
     let flavor: String
     let topping: String?
@@ -21,6 +24,7 @@ struct Cone {
     }
 }
 
+// Step 4
 class IceCreamShop {
     var flavors: [Flavor]
     var sizes: [Size]
@@ -34,6 +38,7 @@ class IceCreamShop {
         self.totalSales = 0.0
     }
     
+    // Step 6
     func orderCone(flavor: String, topping: String?, size: Size) -> Cone {
         let newCone = Cone(flavor: flavor, topping: topping ?? "No Topping", size: size)
         if let coneTopping = topping {
@@ -44,6 +49,7 @@ class IceCreamShop {
     }
 }
 
+// Step 7
 let flavor1 = Flavor(name: "Chocolate Chip Mint", rating: 4.7)
 let flavor2 = Flavor(name: "Chocolate PeanutButter Ripple", rating: 5.0)
 let flavor3 = Flavor(name: "Birthday Cake", rating: 4.9)
@@ -55,6 +61,9 @@ var flavors = [flavor1, flavor2, flavor3, flavor4, flavor5, flavor6]
 
 let toppings = ["Gummy Bears", "Sprinkles", "Whipped Cream", "Nuts", "Oreo Crumbles", "Reeses Crumbles"]
 
+let sizes = [Size.small, Size.medium, Size.large]
+
+// Step 5
 func listTopFlavors() {
     var topFlavors = "Our top flavors are "
     for flavor in flavors {
@@ -65,4 +74,12 @@ func listTopFlavors() {
     print(topFlavors)
 }
 
+// Step 8
+let newIceCreamShop = IceCreamShop(flavors: [flavor1, flavor2, flavor3, flavor4, flavor5, flavor6], sizes: sizes, toppings: toppings.description, totalSales: 0)
+
+// Step 9
 listTopFlavors()
+
+// Step 10
+let newCone = Cone(flavor: flavor5.name, topping: toppings[1], size: Size.large)
+
