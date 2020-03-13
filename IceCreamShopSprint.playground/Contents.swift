2b22acda18b1bc6dd20189b8518f25d716e83697
Stephanie Ballard <user.name>
2020-03-13 09:37:45 -0700

    corrected listTopFlavors func to print correctly

--- IceCreamShopSprint.playground/Contents.swift
+++ IceCreamShopSprint.playground/Contents.swift
@@ -50,11 +50,13 @@ let flavor6 = Flavor(name: "Cookies n Cream", rating: 3.8)
 var flavors = [flavor1, flavor2, flavor3, flavor4, flavor5, flavor6]
 
 func listTopFlavors() {
+    var topFlavors = "Our top flavors are "
     for flavor in flavors {
         if flavor.rating > 4.0 {
-            print("One of our top flavors is \(flavor.name)! ")
+            topFlavors += "\(flavor.name) "
         }
     }
+    print(topFlavors)
 }
 
 listTopFlavors()
